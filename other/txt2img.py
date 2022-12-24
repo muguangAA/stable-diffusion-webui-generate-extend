d5c28e71ae3dc7e7e012bf950e51ec8151f0e9aa
muguangAA <528438806@qq.com>
2022-12-24 23:35:07 +0800

    1

--- other/txt2img.py
+++ other/txt2img.py
@@ -397,64 +397,62 @@ def txt2img(prompt: str, negative_prompt: str, prompt_style: str, prompt_style2:
 
                         for sampler in samplerList:
 
-                            for _ in range(n_iter):
-                                print(
-                                    f"当前第{n + 1}张，sampler:{sampler}, seed:{seed}, prompt: {newPrompt}")
-                                p = StableDiffusionProcessingTxt2Img(
-                                    sd_model=shared.sd_model,
-                                    outpath_samples=opts.outdir_samples or opts.outdir_txt2img_samples,
-                                    outpath_grids=opts.outdir_grids or opts.outdir_txt2img_grids,
-                                    prompt=newPrompt,
-                                    styles=[prompt_style, prompt_style2],
-                                    negative_prompt=negative_prompt,
-                                    seed=seed,
-                                    subseed=subseed,
-                                    subseed_strength=subseed_strength,
-                                    seed_resize_from_h=seed_resize_from_h,
-                                    seed_resize_from_w=seed_resize_from_w,
-                                    seed_enable_extras=seed_enable_extras,
-                                    sampler_name=sampler,
-                                    batch_size=batch_size,
-                                    n_iter=n_iter,
-                                    steps=newSteps,
-                                    cfg_scale=newScale,
-                                    width=newWidth,
-                                    height=newHeight,
-                                    restore_faces=restore_faces,
-                                    tiling=tiling,
-                                    enable_hr=enable_hr,
-                                    denoising_strength=denoising_strength if enable_hr else None,
-                                    firstphase_width=firstphase_width if enable_hr else None,
-                                    firstphase_height=firstphase_height if enable_hr else None,
-                                )
+                            print(
+                                f"当前第{n + 1}张，sampler:{sampler}, seed:{seed}, prompt: {newPrompt}")
+                            p = StableDiffusionProcessingTxt2Img(
+                                sd_model=shared.sd_model,
+                                outpath_samples=opts.outdir_samples or opts.outdir_txt2img_samples,
+                                outpath_grids=opts.outdir_grids or opts.outdir_txt2img_grids,
+                                prompt=newPrompt,
+                                styles=[prompt_style, prompt_style2],
+                                negative_prompt=negative_prompt,
+                                seed=seed,
+                                subseed=subseed,
+                                subseed_strength=subseed_strength,
+                                seed_resize_from_h=seed_resize_from_h,
+                                seed_resize_from_w=seed_resize_from_w,
+                                seed_enable_extras=seed_enable_extras,
+                                sampler_name=sampler,
+                                batch_size=batch_size,
+                                n_iter=n_iter,
+                                steps=newSteps,
+                                cfg_scale=newScale,
+                                width=newWidth,
+                                height=newHeight,
+                                restore_faces=restore_faces,
+                                tiling=tiling,
+                                enable_hr=enable_hr,
+                                denoising_strength=denoising_strength if enable_hr else None,
+                                firstphase_width=firstphase_width if enable_hr else None,
+                                firstphase_height=firstphase_height if enable_hr else None,
+                            )
 
-                                p.scripts = modules.scripts.scripts_txt2img
-                                p.script_args = args
+                            p.scripts = modules.scripts.scripts_txt2img
+                            p.script_args = args
 
-                                if cmd_opts.enable_console_prompts:
-                                    print(f"\ntxt2img: {newWidth}", file=shared.progress_print_out)
+                            if cmd_opts.enable_console_prompts:
+                                print(f"\ntxt2img: {newWidth}", file=shared.progress_print_out)
 
-                                processed = modules.scripts.scripts_txt2img.run(p, *args)
+                            processed = modules.scripts.scripts_txt2img.run(p, *args)
 
-                                if processed is None:
-                                    processed = process_images(p)
+                            if processed is None:
+                                processed = process_images(p)
 
-                                p.close()
+                            p.close()
 
-                                shared.total_tqdm.clear()
+                            shared.total_tqdm.clear()
 
-                                generation_info_js = processed.js()
-                                if opts.samples_log_stdout:
-                                    print(generation_info_js)
+                            generation_info_js = processed.js()
+                            if opts.samples_log_stdout:
+                                print(generation_info_js)
 
-                                if opts.do_not_show_images:
-                                    processed.images = []
+                            if opts.do_not_show_images:
+                                processed.images = []
 
-                                # 保存图片
-                                batchSave(processed, newPrompt, negative_prompt, newSteps, newScale, newWidth,
-                                          newHeight, seed,
-                                          sampler)
-                                seed += 1
+                            # 保存图片
+                            batchSave(processed, newPrompt, negative_prompt, newSteps, newScale, newWidth,
+                                      newHeight, seed,
+                                      sampler)
 
     else:
         print("生成且返回")
