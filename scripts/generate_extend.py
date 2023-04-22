8bcafde597f093e7971d56ed5a4e4cf74e6ecf07
muguangAA <528438806@qq.com>
2023-04-22 19:09:45 +0800

    1

--- scripts/generate_extend.py
+++ scripts/generate_extend.py
@@ -27,7 +27,7 @@ def on_ui_tabs():
                                      '"stepsAndScaleList": [[20, 7]],\n' \
                                      '"negative_prompt": "Default",\n' \
                                      '"pixelList": [[768,1024],[1024,768]],\n' \
-                                     '"samplerList": ["Euler", "DPM++ SDE Karras"]\n' \
+                                     '"samplerList": ["DPM adaptive", "Euler", "DPM++ SDE Karras"]\n' \
                                      '}')
 
         with gr.Row():
